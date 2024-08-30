7f762a0eef529670c4812f38178869a52cc5cbe8
Bob Kazan <rdkbob@gmail.com>
2024-08-30 15:34:06 -0400

    Automate apt commands

--- scripts/fpp_install.sh
+++ scripts/fpp_install.sh
@@ -10,7 +10,7 @@ make "SRCDIR=${SRCDIR}"
 wget -O - https://repo.pulsemesh.io/pulsemsh-repo-key.gpg | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf] https://repo.pulsemesh.io stable main" > /etc/apt/sources.list.d/pulsemsh.list'
 sudo apt update
-sudo apt install pulsemesh-connector
+sudo apt install -y pulsemesh-connector
 sudo systemctl enable pulsemesh-connector.service
 
 . ${FPPDIR}/scripts/common
--- scripts/fpp_uninstall.sh
+++ scripts/fpp_uninstall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sudo systemctl disable pulsemesh-connector.service
-sudo apt-get purge pulsemesh-connector
+sudo apt-get purge -y pulsemesh-connector
 sudo rm /etc/apt/sources.list.d/pulsemsh.list
