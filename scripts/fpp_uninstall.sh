38f1dc08d688be963968859f3afb23e9bba37c84
Bob Kazan <rdkbob@gmail.com>
2024-08-31 07:39:07 -0400

    Cleanup

--- scripts/fpp_uninstall.sh
+++ scripts/fpp_uninstall.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-sudo systemctl disable pulsemesh-connector.service
+sudo systemctl --now disable pulsemesh-connector.service
 sudo apt-get purge -y pulsemesh-connector
 sudo rm /etc/apt/sources.list.d/pulsemsh.list
+
+echo "Removed pulsemesh-connector"
