1af9571e1785d4b25060eb68504a26e1654bc058
Bob Kazan <rdkbob@gmail.com>
2024-11-11 07:01:17 -0500

    Add messaging to direct users to new settings page

--- content.php
+++ content.php
@@ -1,5 +1,10 @@
 <div id="global" class="settings">
 <?
-PrintSettingGroup("PulseMeshSettings", "", "", 1, "fpp-pulsemesh");
+        $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? "https://" : "http://";
+        $host = $_SERVER['SERVER_NAME'] ?? 'localhost';
+        $url = $protocol . $host . ":8089";
+        echo '<h1>PulseMesh Settings</h1><p><a href="' . htmlspecialchars($url) . '">Click Here</a> to visit the PulseMesh settings page to configure the token or debug the PulseMesh connector.</p><p>Note - don\'t use the legacy settings below unless specifically directed to do so.</p>';
+        
+        PrintSettingGroup("PulseMeshSettings", "", "", 1, "fpp-pulsemesh");
 ?>
 </div>
